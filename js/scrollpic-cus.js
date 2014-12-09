99d3c50aa41f49be082d91a008a5b4db826d0b67
hhhome <chh2009@vip.qq.com>
2014-12-09 23:38:41 +0800

    third commit

--- js/scrollpic-cus.js
+++ js/scrollpic-cus.js
@@ -1,11 +1,14 @@
-function ScrollPic(sContentId){
+function ScrollPic(sContentId, prevId, nextId){
 	this.sContentId = sContentId;
-	this.prev = prev;
-	this.next = next;
+	this.prevId = prevId;
+	this.nextId = nextId;
 	this.frameWidth = 0;
-	this.cLayout = document.createElement("div");
-	this.cItem01 = document.createElement("div");
-	this.cItem02 = document.createElement("div");
+	this.everyWidth = 0;
+	if (!ScrollPic.childs) {
+        ScrollPic.childs = []
+    };
+    this.ID = ScrollPic.childs.length;
+    ScrollPic.childs.push(this);
 }
 
 ScrollPic.prototype={
@@ -21,10 +24,24 @@ ScrollPic.prototype={
             throw new Error("sContentId不是正确的对象.(sContentId = \"" + this.sContentId + "\")");
             return
         };
-
         this.initLayout();
+		
+		
+		if(this.prevId){
+			this.prevObj = jBase.$$(this.prevId);
+			
+			jBase.addEvent(this.prevObj,'mousedown',Function("ScrollPic.childs[" + this.ID + "].move()"));
+		}
+		if(this.nextId){
+			this.nextObj = jBase.$$(this.nextId);
+			//jBase.addEvent(this.nextObj,'mousedown',this.move());
+		}
 	},
 	initLayout: function(){
+		this.cLayout = document.createElement("div");
+		this.cItem01 = document.createElement("div");
+	 	this.cItem02 = document.createElement("div");
+		
 		this.sContentDiv.style.width = this.frameWidth + 'px';
 		this.sContentDiv.style.overflow = "hidden";
 		this.cItem01.innerHTML = this.cItem02.innerHTML = this.sContentDiv.innerHTML;
@@ -41,8 +58,14 @@ ScrollPic.prototype={
 		this.cItem01.style.display = 'inline';
 		this.cItem02.style.cssFloat = 'left';
 		this.cItem02.style.display = 'inline';
+	},
+	move: function(){
+		//alert(this.sContentDiv.scrollLeft);
+		if (this.sContentDiv.scrollLeft - this.everyWidth <= 0) {
+			alert(11);
+            this.sContentDiv.scrollLeft = this.cItem01.scrollWidth + this.sContentDiv.scrollLeft - this.everyWidth
+        }
 	}
-
 	
 
 
